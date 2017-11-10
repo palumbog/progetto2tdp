a26a3c3344b2370ee07933d00c302a4d11136ae7
giuseppe palumbo <pepomast21@hotmail.it>
2017-11-10 12:06:16 +0100

    Eserc 2.1
    2.1

--- pkg_1/1.py
+++ pkg_1/1.py
@@ -3,7 +3,11 @@ import random
 import time
 
 
-num = 10000
+"""1) Ricerca più efficiente in AVL rispetto ai RB sempre
+   2) Hp: dati in input ordinati o quasi ordinati
+      Th: costo complessivo degli inserimenti minore in  AVL"""
+
+num = 1000
 randnum = 10000
 
 def t():
@@ -11,21 +15,89 @@ def t():
 
 avltree = avl_tree.AVLTreeMap()
 rbtree = red_black_tree.RedBlackTreeMap()
-keys = []
+keys = {}
 
-t_insert_AVL = 0
-t_insert_RB = 0
+def flush():
+    avltree.clear()
+    rbtree.clear()
+    keys.clear()
 
-print("Random Insert")
+
+
+t_insert_random_AVL = 0
+t_insert_random_RB = 0
+
+print("Inserimento Disordinato")
 for i in range(num):
     key = random.randint(0,randnum)
-    keys.append(key)
     v = random.randint(0,randnum)
+    keys[key] = v
     start = t()
     avltree[key] = v
     stop = t()
     delta = stop - start
-    t_insert_AVL += delta
+    t_insert_random_AVL += delta
+    #print("\nkey "+str(key) + " value "+str(v))
+    #print(str(i+1)+"\nInsert AVL "+str(delta))
+    start = t()
+    rbtree[key] = v
+    stop = t()
+    delta = stop - start
+    t_insert_random_RB += delta
+    #print("Insert RB " + str(delta))
+print("Altezza AVL "+ str(avltree.height()))
+print("Altezza RB "+ str(rbtree.height()))
+print("Aggiunti " + str(len(avltree)) +" elementi")
+print("Num elementi keys " + str(len(keys)))
+print("Tempo INSERT AVL " + str(t_insert_random_AVL))
+print("Tempo INSERT RB  " + str(t_insert_random_RB))
+
+
+t_search_random_AVL = 0
+t_search_random_RB = 0
+
+print("\nRicerca inserimenti disordinati")
+for key in keys:
+    AVLstart = t()
+    avltree.find_position(key)
+    AVLstop = t()
+
+    #print("Found " + str(pavl.value()))
+    avl_part_time = AVLstop - AVLstart
+
+    RBstart = t()
+    rbtree.find_position(key)
+    RBstop = t()
+
+    #print("Found " + str(pavl.value()))
+    rb_part_time = RBstop - RBstart
+
+    t_search_random_AVL += avl_part_time
+    t_search_random_RB += rb_part_time
+
+delta_search_random = t_search_random_AVL - t_search_random_RB
+
+print("Tempo SEARCH AVL --> "+str(t_search_random_AVL))
+print("Tempo SEARCH RB  --> "+str(t_search_random_RB))
+
+
+
+flush()
+t_insert_ordinato_AVL = 0
+t_insert_ordinato_RB = 0
+
+
+
+print("\nInserimento ordinato")
+for i in range(num):
+    key = i
+    v = random.randint(0,randnum)
+    keys[key] = v
+    start = t()
+    avltree[key] = v
+    stop = t()
+    delta = stop - start
+    t_insert_ordinato_AVL += delta
     #print("\nkey "+str(key) + " value "+str(v))
     #print(str(i+1)+"\nInsert AVL "+str(delta))
     v = random.randint(0, randnum)
@@ -33,52 +105,62 @@ for i in range(num):
     rbtree[key] = v
     stop = t()
     delta = stop - start
-    t_insert_RB += delta
+    t_insert_ordinato_RB += delta
     #print("Insert RB " + str(delta))
-print("Num elementi AVL " + str(len(avltree)))
-print("Num elementi RB " + str(len(rbtree)))
 print("Altezza AVL "+ str(avltree.height()))
 print("Altezza RB "+ str(rbtree.height()))
 print("Aggiunti " + str(len(avltree)) +" elementi")
-print("Tempo INSERT AVL " + str(t_insert_AVL))
-print("Tempo INSERT RB  " + str(t_insert_RB))
+print("Num elementi keys " + str(len(keys)))
+print("Tempo INSERT AVL " + str(t_insert_ordinato_AVL))
+print("Tempo INSERT RB  " + str(t_insert_ordinato_RB))
 
 
-t_search_AVL = 0
-t_search_RB = 0
+t_search_ordinato_AVL = 0
+t_search_ordinato_RB = 0
 
+print("\nRicerca inserimenti ordinati")
 for key in keys:
     AVLstart = t()
-    pavl = avltree.find_position(key)
+    avltree.find_position(key)
     AVLstop = t()
     #print("Found " + str(pavl.value()))
     avl_part_time = AVLstop - AVLstart
 
     RBstart = t()
-    prb = rbtree.find_position(key)
+    rbtree.find_position(key)
     RBstop = t()
     #print("Found " + str(pavl.value()))
     rb_part_time = RBstop - RBstart
 
-    t_search_AVL += avl_part_time
-    t_search_RB += rb_part_time
+    t_search_ordinato_AVL += avl_part_time
+    t_search_ordinato_RB += rb_part_time
 
-print("AVL --> "+str(t_search_AVL))
-print("RB --> "+str(t_search_RB))
+delta_search_ordinato = t_search_random_AVL - t_search_random_RB
 
-avltree.clear()
-rbtree.clear()
+print("Tempo SEARCH AVL --> "+str(t_search_ordinato_AVL))
+print("Tempo SEARCH RB  --> "+str(t_search_ordinato_RB))
 
-print("Ordered Insert")
+
+
+flush()
+
+t_insert_qordinato_AVL = 0
+t_insert_qordinato_RB = 0
+
+print("\nInserimento quasi ordinato")
+key = 0
 for i in range(num):
-    key = i
-    keys.append(key)
+    if(i%100 == 0):
+        key = random.randint(0, randnum)
+    else:
+        key = i
     v = random.randint(0,randnum)
+    keys[key] = v
     start = t()
     avltree[key] = v
     stop = t()
     delta = stop - start
-    t_insert_AVL += delta
+    t_insert_qordinato_AVL += delta
     #print("\nkey "+str(key) + " value "+str(v))
     #print(str(i+1)+"\nInsert AVL "+str(delta))
     v = random.randint(0, randnum)
@@ -86,12 +168,51 @@ for i in range(num):
     rbtree[key] = v
     stop = t()
     delta = stop - start
-    t_insert_RB += delta
+    t_insert_qordinato_RB += delta
     #print("Insert RB " + str(delta))
-print("Num elementi AVL " + str(len(avltree)))
-print("Num elementi RB " + str(len(rbtree)))
 print("Altezza AVL "+ str(avltree.height()))
 print("Altezza RB "+ str(rbtree.height()))
 print("Aggiunti " + str(len(avltree)) +" elementi")
-print("Tempo INSERT AVL " + str(t_insert_AVL))
-print("Tempo INSERT RB  " + str(t_insert_RB))
+print("Num elementi keys " + str(len(keys)))
+print("Tempo INSERT AVL " + str(t_insert_qordinato_AVL))
+print("Tempo INSERT RB  " + str(t_insert_qordinato_RB))
+
+t_search_qordinato_AVL = 0
+t_search_qordinato_RB = 0
+
+print("\nRicerca inserimenti quasi ordinati")
+for key in keys:
+    AVLstart = t()
+    avltree.find_position(key)
+    AVLstop = t()
+    #print("Found " + str(pavl.value()))
+    avl_part_time = AVLstop - AVLstart
+
+    RBstart = t()
+    rbtree.find_position(key)
+    RBstop = t()
+    #print("Found " + str(pavl.value()))
+    rb_part_time = RBstop - RBstart
+
+    t_search_qordinato_AVL += avl_part_time
+    t_search_qordinato_RB += rb_part_time
+
+delta_search_qordinato = t_search_qordinato_AVL - t_search_qordinato_RB
+
+print("Tempo SEARCH AVL --> "+str(t_search_qordinato_AVL))
+print("Tempo SEARCH RB  --> "+str(t_search_qordinato_RB))
+
+
+
+
+delta_insert_random = t_insert_random_AVL - t_insert_random_RB
+delta_insert_ordinato = t_insert_ordinato_AVL- t_insert_ordinato_RB
+delta_insert_qordinato = t_insert_qordinato_AVL - t_insert_qordinato_RB
+
+print("\nSe valore negativo è più veloce AVL")
+print("Insert Random          " + str(delta_insert_random))
+print("Insert Ordinato       " + str(delta_insert_ordinato))
+print("Insert Quasi Ordinato " + str(delta_insert_qordinato))
+print("Search Disordinato    " + str(delta_search_random))
+print("Search Ordinato       " + str(delta_search_ordinato))
+print("Search Quasi Ordinato " + str(delta_search_qordinato) + "\n")
--- pkg_2/MyTreeMap.py
+++ pkg_2/MyTreeMap.py
@@ -1,10 +1,133 @@
 from TdP_collections.map.binary_search_tree import TreeMap
 
 class MyTreeMap(TreeMap):
+    # ---------------------------- override Position class ----------------------------
+    class Position(TreeMap.Position):
+        def __init__(self,container, node, before = None, after = None,):
+            #super(Position, self, container,node).__init__()
+            self._before = before
+            self._after = after
+            self._container = container
+            self._node = node
+
+    def after(self,p):
+        self._validate(p)
+        return p._after
+
+    def before(self,p):
+        self._validate(p)
+        return p._before
+
+    def _make_position(self, node):
+        """Return Position instance for given node (or None if no node)."""
+        return self.Position(self, node) if node is not None else None
 
 
-    def after(self):
-        return
+    def delete(self, p):
+        """Remove the item at given Position."""
+        self._validate(p)  # inherited from LinkedBinaryTree
+        if self.left(p) and self.right(p):  # p has two children
+            replacement = self._subtree_last_position(self.left(p))
+            self._replace(p, replacement.element())  # from LinkedBinaryTree
+            p = replacement
+        # now p has at most one child
+        parent = self.parent(p)
+        self._delete(p)  # inherited from LinkedBinaryTree
+        self._rebalance_delete(parent)  # if root deleted, parent is None
 
-    def before(self):
-        return
+        # --------------------- public methods for (standard) map interface ---------------------
+
+    def __getitem__(self, k):
+        """Return value associated with key k (raise KeyError if not found)."""
+        if self.is_empty():
+            raise KeyError('Key Error: ' + repr(k))
+        else:
+            p = self._subtree_search(self.root(), k)
+            self._rebalance_access(p)  # hook for balanced tree subclasses
+            if k != p.key():
+                raise KeyError('Key Error: ' + repr(k))
+            return p.value()
+
+    def __setitem__(self, k, v):
+        """Assign value v to key k, overwriting existing value if present."""
+        """Mi serve la position dell'elemento salvato"""
+        position = None
+        if self.is_empty():
+            leaf = self._add_root(self._Item(k, v))  # from LinkedBinaryTree
+
+            self._set_a_b(leaf)
+
+        else:
+            p = self._subtree_search(self.root(), k)
+            if p.key() == k:
+                p.element()._value = v  # replace existing item's value
+                self._rebalance_access(p)  # hook for balanced tree subclasses
+
+                self._set_a_b(p)
+
+                return
+            else:
+                item = self._Item(k, v)
+                if p.key() < k:
+                    leaf = self._add_right(p, item)  # inherited from LinkedBinaryTree
+                    self._set_a_b(leaf)
+                else:
+                    leaf = self._add_left(p, item)  # inherited from LinkedBinaryTree
+                    self._set_a_b(leaf)
+
+        self._rebalance_insert(leaf)  # hook for balanced tree subclasses
+
+    def __delitem__(self, k):
+        """Remove item associated with key k (raise KeyError if not found)."""
+        if not self.is_empty():
+            p = self._subtree_search(self.root(), k)
+            if k == p.key():
+                self.delete(p)  # rely on positional version
+                return  # successful deletion complete
+            self._rebalance_access(p)  # hook for balanced tree subclasses
+        raise KeyError('Key Error: ' + repr(k))
+
+    def _set_a_b(self, p, a, b):
+        p._before = super(MyTreeMap, self).before(p)
+        p._after = super(MyTreeMap, self).after(p)
+
+
+from TdP_collections.map.binary_search_tree import TreeMap
+import random
+import time
+
+def t():
+    return time.time()
+
+num = 10000
+randnum = 10000
+mytreemap = MyTreeMap()
+treemap  = TreeMap()
+keys = {}
+
+t_insert_MTM = 0
+t_insert_TM = 0
+
+print("Inserimento Disordinato")
+for i in range(num):
+    key = random.randint(0,randnum)
+    v = random.randint(0,randnum)
+    keys[key] = v
+    #MyTreeMap
+    start = t()
+    mytreemap[key] = v
+    stop = t()
+    delta = stop - start
+    t_insert_MTM += delta
+    #TreeMap
+    start = t()
+    treemap[key] = v
+    stop = t()
+    delta = stop - start
+    t_insert_TM += delta
+print("Altezza MTM "+ str(mytreemap.height()))
+print("Altezza TM "+ str(treemap.height()))
+print("Aggiunti " + str(len(mytreemap)) +" elementi")
+print("Num elementi keys " + str(len(keys)))
+print("Tempo INSERT MTM " + str(t_insert_MTM))
+print("Tempo INSERT TM  " + str(t_insert_TM))
